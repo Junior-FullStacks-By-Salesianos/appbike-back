09f9f9918c8a8634c799d132381072a77dc03a65
Alexander Luque <114216624+alexluque02@users.noreply.github.com>
2023-11-17 14:25:14 +0100

    Proyecto creado con dependencias

---
+++ appbike/src/main/java/com/salesianos/triana/appbike/AppbikeApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.appbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbikeApplication.class, args);
+	}
+
+}
---
+++ appbike/src/test/java/com/salesianos/triana/appbike/AppbikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.appbike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppbikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
