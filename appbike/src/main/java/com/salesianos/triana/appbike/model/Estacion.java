045c9b38319a20e24323e1db8cdedf6cf7dd3788
Alexander Luque <114216624+alexluque02@users.noreply.github.com>
2023-11-20 13:34:00 +0100

    creating data model

---
+++ appbike/src/main/java/com/salesianos/triana/appbike/model/Estacion.java
@@ -0,0 +1,33 @@
+package com.salesianos.triana.appbike.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class Estacion {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long numero;
+    private String nombre;
+    private String coordenadas;
+    private int capacidad;
+
+    @OneToMany(mappedBy = "estacion")
+    private Set<Bicicleta> bicicletas;
+
+    @OneToMany(mappedBy = "estacion")
+    private List<Uso> usos;
+}
