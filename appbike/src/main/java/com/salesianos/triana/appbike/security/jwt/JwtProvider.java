9d6e42ecb779ed7bcca7df7eb020fb88b4843ef2
Alexander Luque <114216624+alexluque02@users.noreply.github.com>
2023-11-21 14:27:46 +0100

    have to resolve security problems

---
+++ appbike/src/main/java/com/salesianos/triana/appbike/security/jwt/JwtProvider.java
@@ -0,0 +1,88 @@
+package com.salesianos.triana.appbike.security.jwt;
+
+
+import com.salesianos.triana.appbike.model.Usuario;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.java.Log;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.security.SignatureException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.UUID;
+
+@Log
+@Service
+public class JwtProvider {
+    public static final String TOKEN_TYPE = "JWT";
+    public static final String TOKEN_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.duration}")
+    private int jwtLifeInDays;
+
+    private JwtParser jwtParser;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+
+        secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+        jwtParser = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build();
+    }
+
+
+    public String generateToken(Authentication authentication) {
+
+        Usuario user = (Usuario) authentication.getPrincipal();
+
+        Date tokenExpirationDateTime =
+                Date.from(
+                        LocalDateTime
+                                .now()
+                                .plusDays(jwtLifeInDays)
+                                .atZone(ZoneId.systemDefault())
+                                .toInstant()
+                );
+
+        return Jwts.builder()
+                .setHeaderParam("typ", TOKEN_TYPE)
+                .setSubject(user.getId().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(tokenExpirationDateTime)
+                .signWith(secretKey)
+                .compact();
+
+    }
+
+    public UUID getUserIdFromJwtToken(String token) {
+        return UUID.fromString(
+                jwtParser.parseClaimsJws(token).getBody().getSubject()
+        );
+    }
+
+
+    public boolean validateToken(String token) {
+
+        try {
+            jwtParser.parseClaimsJws(token);
+            return true;
+        } catch (SignatureException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
+            log.info("Error con el token: " + ex.getMessage());
+        }
+        return false;
+
+    }
+}
