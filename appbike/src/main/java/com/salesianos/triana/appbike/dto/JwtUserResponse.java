cd669441298e67073b22add5d3079fe1e6be085a
alexluque02 <alexanderluquehoffrogge@gmail.com>
2023-11-21 22:16:28 +0100

    register anf login funtionate ok without validation

---
+++ appbike/src/main/java/com/salesianos/triana/appbike/dto/JwtUserResponse.java
@@ -0,0 +1,34 @@
+package com.salesianos.triana.appbike.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.salesianos.triana.appbike.model.Usuario;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class JwtUserResponse extends UserResponse {
+
+    private String token;
+    private String refreshToken;
+
+    public JwtUserResponse(UserResponse userResponse) {
+        id = userResponse.getId();
+        username = userResponse.getUsername();
+        nombre = userResponse.getNombre();
+        email = userResponse.getEmail();
+        createdAt = userResponse.getCreatedAt();
+    }
+
+    public static JwtUserResponse of (Usuario user, String token) {
+        JwtUserResponse result = new JwtUserResponse(UserResponse.of(user));
+        result.setToken(token);
+        return result;
+
+    }
+
+}
