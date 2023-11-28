9e8b4407c4f2b7e58a6a091a4bd1888426a70f08
Cristian <pulidocabellochristian@gmail.com>
2023-11-28 10:03:51 +0100

    Funcionalidad delete completa

--- appbike/src/main/java/com/salesianos/triana/appbike/repository/EstacionRepository.java
+++ appbike/src/main/java/com/salesianos/triana/appbike/repository/EstacionRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EstacionRepository extends JpaRepository<Estacion, UUID> {
+
+
+    Optional<Estacion> findByNombre(String s);
 
     @Query("SELECT COUNT(b) FROM Bicicleta b WHERE b.estacion.numero = :numeroEstacion")
     int countBikesInAStationForUuid(@Param("numeroEstacion") Long numeroEstacion);
