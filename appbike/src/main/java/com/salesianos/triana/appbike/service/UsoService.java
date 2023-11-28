913b02fb3cfff628da51d9dc70c48d3dda3c02c4
tomasdelpinorep <tomasdelpino28@gmail.com>
2023-11-28 14:12:03 +0100

    uses by user

--- appbike/src/main/java/com/salesianos/triana/appbike/service/UsoService.java
+++ appbike/src/main/java/com/salesianos/triana/appbike/service/UsoService.java
@@ -14,6 +14,7 @@ import java.nio.file.AccessDeniedException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -87,5 +88,9 @@ public class UsoService {
         toFinish.setCoste(difference * coste.getPrecioMinuto()); //Entity Cost
 
         return usoRepository.save(toFinish);
+    }
+
+    public List<Uso> findUsoByUser(UUID userId){
+        return usoRepository.findByAuthor(userId.toString());
     }
 }
