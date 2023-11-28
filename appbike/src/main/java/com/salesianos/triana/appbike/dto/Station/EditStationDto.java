96b8f20bf5f05a130446785e4d714e7483425d89
cpcx04 <pulidocabellochristian@gmail.com>
2023-11-28 19:45:22 +0100

    falta carrousel

--- appbike/src/main/java/com/salesianos/triana/appbike/dto/Station/EditStationDto.java
+++ appbike/src/main/java/com/salesianos/triana/appbike/dto/Station/EditStationDto.java
@@ -13,7 +13,7 @@ public record EditStationDto(
         @Pattern(regexp = "^[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?),\\s*[-+]?(180(\\.0+)?|((1[0-7]\\d)|([1-9]?\\d))(\\.\\d+)?)$")
         @NotNull
         String coordenadas,
-        @Min(1)
+        @Min(0)
         @Max(31)
         @NotNull
         int capacidad
